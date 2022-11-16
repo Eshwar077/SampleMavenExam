27a6fdc7fcde8377e094d01ee54289ad92567dcc
nitya <nittu4frnds@gmail.com>
2022-11-16 12:24:16 +0530

    sent

---
+++ src/main/java/SampleMavenExam/presentproject/App.java
@@ -0,0 +1,13 @@
+package SampleMavenExam.presentproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "THIS IS SE LAB INTERNAL 1 EXAM" );
+    }
+}
